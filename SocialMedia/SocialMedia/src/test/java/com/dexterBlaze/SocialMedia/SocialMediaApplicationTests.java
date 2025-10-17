76e353fa729a090d38540a3d5be888a138c9b93b
dexter-blaze <int.abhisingh02@gmail.com>
2025-10-18 01:25:01 +0530

    added SocialMedia

---
+++ SocialMedia/SocialMedia/src/test/java/com/dexterBlaze/SocialMedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexterBlaze.SocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
