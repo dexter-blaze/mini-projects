d280493e59d170f4073a3e1269f6a34f5228c020
dexter-blaze <int.abhisingh02@gmail.com>
2025-11-01 08:31:28 +0530

    use ModelMap for dynamically taking query param

--- Week_2/MVC_Core/website/src/main/java/com/dexterBlaze/website/web/SupportController.java
+++ Week_2/MVC_Core/website/src/main/java/com/dexterBlaze/website/web/SupportController.java
@@ -7,6 +7,7 @@ import com.dexterBlaze.website.service.UserMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,12 +18,13 @@ public class SupportController {
 
     //request mapping -> mapping this function to a url path /support matlab /support jb request ho to ye function chalao
     @RequestMapping("/support")
-    public String getSupportPage(@RequestParam("id") String id, Model uiModel) {
+    public String getSupportPage(@RequestParam("id") String id, ModelMap map) {
         //Integer.parseInt -> takes a string and converts it to its value. Eg. String x = "123" converted to no. 123
-        String finalUserMessage = userMessageService.createMessage(Integer.parseInt(id));
+//        String finalUserMessage = userMessageService.createMessage(Integer.parseInt(id));
         //basically we passing the reference to the instance variable message
         //we are sending data to the jsp here
-        uiModel.addAttribute("message", finalUserMessage);
+//        uiModel.addAttribute("message", finalUserMessage);
+        map.addAttribute("message", "Hi, welcome to support page "+id);
         return "support";
     }
 }
--- Week_2/MVC_Core/website/src/main/webapp/WEB-INF/jsp/support.jsp
+++ Week_2/MVC_Core/website/src/main/webapp/WEB-INF/jsp/support.jsp
@@ -1,4 +1,4 @@
 <h1>Support Page</h1>
 <div>${message}</div>
 <% String a = "abhee is using java here"; %>
-<%=a %>
+<%= a%>
