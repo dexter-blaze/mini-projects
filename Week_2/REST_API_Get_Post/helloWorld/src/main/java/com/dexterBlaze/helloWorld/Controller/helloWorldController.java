639d4a313c6a83eaca507fdf28995fdde779212a
dexter-blaze <int.abhisingh02@gmail.com>
2025-11-03 15:04:18 +0530

    add a variable to our url and implement @PathVariable

--- Week_2/REST_API_Get_Post/helloWorld/src/main/java/com/dexterBlaze/helloWorld/Controller/helloWorldController.java
+++ Week_2/REST_API_Get_Post/helloWorld/src/main/java/com/dexterBlaze/helloWorld/Controller/helloWorldController.java
@@ -1,6 +1,7 @@
 package com.dexterBlaze.helloWorld.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class helloWorldController {
     @GetMapping("hello-earth")
     public String helloEarth() {
         return "Hi earth! You got a new coder...";
+    }
+
+    @GetMapping("hello-mars/{name}")
+    public String helloFromMars(@PathVariable String name) {
+        return "Hello! from Mars " + name;
     }
 }
